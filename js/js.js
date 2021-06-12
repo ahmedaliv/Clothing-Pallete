2a38519526295536c3ea8414eba1bb835a2121c8
ahmedmalii <65877525+ahmedmalii@users.noreply.github.com>
2021-06-12 19:59:19 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,28 +1,31 @@
-const button= document.querySelectorAll(".btt"); 
 const section=document.querySelector('section');
-button[0].addEventListener('click',function(){
-    section.style.backgroundImage='url("./images/1.jpg")';
-    button[1].style.opacity="0.5";  
-    button[0].style.opacity="1";
 
-    button[2].style.opacity="0.5";
-})
-button[1].addEventListener('click',function(){
-    section.style.backgroundImage='url("./images/2.jpg")';
 
-    button[1].style.opacity="1";
-    button[0].style.opacity="0.5";
-    button[2].style.opacity="0.5";
-    });
-button[2].addEventListener('click',function(){
-        section.style.backgroundImage='url("./images/3.jpg")';
+// const button= document.querySelectorAll(".btt"); 
+// button[1].addEventListener('click',function(){
+//   section.style.backgroundImage='url("./images/2.jpg")';
+
+//   button[1].style.opacity="1";
+//   button[0].style.opacity="0.5";
+//   button[2].style.opacity="0.5";
+//   });
+// button[0].addEventListener('click',function(){
+//     section.style.backgroundImage='url("./images/1.jpg")';
+//     button[1].style.opacity="0.5";  
+//     button[0].style.opacity="1";
+
+//     button[2].style.opacity="0.5";
+// })
+
+// button[2].addEventListener('click',function(){
+//         section.style.backgroundImage='url("./images/3.jpg")';
     
-        button[2].style.opacity="1";
-        button[1].style.opacity="0.5";
-        button[0].style.opacity="0.5";
-        });
+//         button[2].style.opacity="1";
+//         button[1].style.opacity="0.5";
+//         button[0].style.opacity="0.5";
+//         });
 
-        ///////end buttons js/////////
+//         ///////end buttons js/////////
 
 
     
@@ -46,3 +49,55 @@ button[2].addEventListener('click',function(){
               }
             }
           }
+          const forward=document.querySelector('.BUTFOR button');
+          const back=document.querySelector('.BUTback button');
+          const backimages=['./images/1.jpg' , './images/2.jpg', './images/3.jpg']
+          let i=0
+
+          let url= "background-image: url(\"" + backimages[i] +"\");";
+
+          forward.addEventListener('click', function chbgim(){
+            if(section.getAttribute('style')==null || i==0){
+              i++;
+              url= "background-image: url(\"" + backimages[i] +"\");";
+            section.setAttribute('style' , url)
+            
+            }
+            else if(i==1){
+              i++;
+
+             url= "background-image: url(\"" + backimages[i] +"\");";
+             section.setAttribute('style' , url)
+            }
+
+            else if(i==2 ) {
+              i=0;
+             url= "background-image: url(\"" + backimages[i] +"\");";
+           section.setAttribute('style' , url);
+              
+            }
+
+
+          
+          })
+          back .addEventListener('click' , function chbgim2() {
+            if(section.getAttribute('style')==null || i==0){
+              i+=2;
+              url= "background-image: url(\"" + backimages[i] +"\");";
+            section.setAttribute('style' , url)
+            
+            }
+            else if(i==2){
+              i--;
+
+             url= "background-image: url(\"" + backimages[i] +"\");";
+             section.setAttribute('style' , url)
+            }
+            else if(i==1 ) {
+              i=0;
+             url= "background-image: url(\"" + backimages[i] +"\");";
+           section.setAttribute('style' , url);
+              
+            }
+
+          })
